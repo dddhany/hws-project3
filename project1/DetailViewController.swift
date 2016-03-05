2bffc7de931d9f0dd1c0c92cfc474c06b9fb491d
Dhany Basuki <dddhany+github@gmail.com>
2016-03-05 22:28:10 +0800

    first commit

---
+++ project1/DetailViewController.swift
@@ -0,0 +1,55 @@
+//
+//  DetailViewController.swift
+//  project1
+//
+//  Created by Dhany Basuki on 3/3/16.
+//  Copyright © 2016 Dhany. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+
+    @IBOutlet weak var detailImageView: UIImageView!
+
+    var detailItem: String? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let imageView = self.detailImageView {
+                imageView.image = UIImage(named: detail)
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+
+}
+
